88ff829ef371d5faf3ab60f89c71a11214d85eec
Toni322 <mehanik13@ukr.net>
2016-11-05 11:11:58 +0200

    Gogla_Auth_Work

--- app/src/main/java/com/examplenasa/roma/mytestapp/MainActivity.java
+++ app/src/main/java/com/examplenasa/roma/mytestapp/MainActivity.java
@@ -1,22 +1,29 @@
 package com.examplenasa.roma.mytestapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
 SignInButton signInButton;
-    Button signtOutButton;
+    Button signOutButton;
     TextView statusTextView;
     GoogleApiClient mGoogleApiClient;
     public  static final String TAG = "SignActivity";
@@ -34,28 +41,59 @@ SignInButton signInButton;
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
 
-        statusTextView = (TextView) findViewById(R.id.status_text);
-        signInButton = (SignInButton) findViewById(R.id.sing_in_button);
+        statusTextView = (TextView) findViewById(R.id.textView);
+        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
         signInButton.setOnClickListener(this);
 
-        signtOutButton = (Button) findViewById(R.id.logout_button);
-        signtOutButton.setOnClickListener(this);
+        signOutButton = (Button) findViewById(R.id.logout_button);
+        signOutButton.setOnClickListener(this);
     }
     private void signIn(){
         Intent signIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signIntent,RC_SIGN_IN);
 
     }
+    private void signOut(){
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                statusTextView.setText("Bye");
+            }
+        });
+    }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.sing_in_button;
+            case R.id.sign_in_button:
             signIn();
              break;
-         case R.id.logout_button;
-
+            case R.id.logout_button:
+            signOut();
              break;
+        }
+    }
+
+    private void handleSignResult(GoogleSignInResult result){
+        Log.d(TAG,"handleSignResult" + result.isSuccess());
+        if(result.isSuccess()){
+            GoogleSignInAccount acc = result.getSignInAccount();
+            statusTextView.setText("Zdarowa "+ acc.getDisplayName());
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG,"onConnectionFailed" + connectionResult);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignResult(result);
         }
     }
 }
