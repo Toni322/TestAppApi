1f96d7f5b4e7885b4a79e02b17ce5a5525c906b1
Toni322 <mehanik13@ukr.net>
2016-11-05 18:24:16 +0200

    facebookErr

--- app/src/main/java/com/examplenasa/roma/mytestapp/MainActivity.java
+++ app/src/main/java/com/examplenasa/roma/mytestapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.examplenasa.roma.mytestapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,7 +9,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import com.facebook.FacebookSdk;
 
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -26,6 +32,8 @@ SignInButton signInButton;
     Button signOutButton;
     TextView statusTextView;
     GoogleApiClient mGoogleApiClient;
+    LoginButton loginButton;
+
     public  static final String TAG = "SignActivity";
     public  static final int RC_SIGN_IN = 9001;
 
@@ -33,21 +41,47 @@ SignInButton signInButton;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());//facebook
+
+
+
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
+        // ATTENTION: This "addApi(AppIndex.API)"was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
         mGoogleApiClient = new GoogleApiClient.Builder(this)
-            .enableAutoManage(this, this)
+                .enableAutoManage(this, this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
+                .addApi(AppIndex.API).build();
 
+        //Google
         statusTextView = (TextView) findViewById(R.id.textView);
         signInButton = (SignInButton) findViewById(R.id.sign_in_button);
         signInButton.setOnClickListener(this);
 
         signOutButton = (Button) findViewById(R.id.logout_button);
         signOutButton.setOnClickListener(this);
+
+        //Facebook
+        //loginButton = (LoginButton) findViewById(R.id.sign_facebook_button);
+
+
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            case R.id.logout_button:
+                signOut();
+                break;
+        }
+    }
+
     private void signIn(){
         Intent signIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signIntent,RC_SIGN_IN);
@@ -62,20 +96,9 @@ SignInButton signInButton;
         });
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.sign_in_button:
-            signIn();
-             break;
-            case R.id.logout_button:
-            signOut();
-             break;
-        }
-    }
 
     private void handleSignResult(GoogleSignInResult result){
-        Log.d(TAG,"handleSignResult" + result.isSuccess());
+        Log.d(TAG,"handleSignResult " + result.isSuccess());
         if(result.isSuccess()){
             GoogleSignInAccount acc = result.getSignInAccount();
             statusTextView.setText("Zdarowa "+ acc.getDisplayName());
@@ -84,7 +107,7 @@ SignInButton signInButton;
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Log.d(TAG,"onConnectionFailed" + connectionResult);
+        Log.d(TAG,"onConnectionFailed " + connectionResult);
     }
 
     @Override
@@ -95,5 +118,41 @@ SignInButton signInButton;
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             handleSignResult(result);
         }
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        mGoogleApiClient.connect();
+        AppIndex.AppIndexApi.start(mGoogleApiClient, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(mGoogleApiClient, getIndexApiAction());
+        mGoogleApiClient.disconnect();
     }
 }
