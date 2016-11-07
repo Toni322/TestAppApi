ef35449e682fc7b31f67e490f6b54a6a2122503f
Toni322 <mehanik13@ukr.net>
2016-11-07 02:10:27 +0200

    day3Twitter

--- app/src/main/java/com/examplenasa/roma/mytestapp/MainActivity.java
+++ app/src/main/java/com/examplenasa/roma/mytestapp/MainActivity.java
@@ -1,158 +1,23 @@
 package com.examplenasa.roma.mytestapp;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.support.annotation.NonNull;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import com.facebook.FacebookSdk;
-
-import com.facebook.login.widget.LoginButton;
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
 
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
-SignInButton signInButton;
-    Button signOutButton;
-    TextView statusTextView;
-    GoogleApiClient mGoogleApiClient;
-    LoginButton loginButton;
+public class MainActivity extends AppCompatActivity {
 
-    public  static final String TAG = "SignActivity";
-    public  static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        FacebookSdk.sdkInitialize(getApplicationContext());//facebook
 
 
-
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-        // ATTENTION: This "addApi(AppIndex.API)"was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .addApi(AppIndex.API).build();
-
-        //Google
-        statusTextView = (TextView) findViewById(R.id.textView);
-        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
-        signInButton.setOnClickListener(this);
-
-        signOutButton = (Button) findViewById(R.id.logout_button);
-        signOutButton.setOnClickListener(this);
-
-        //Facebook
-        //loginButton = (LoginButton) findViewById(R.id.sign_facebook_button);
+        Intent intent = new Intent(this, Authorization.class);
+        startActivity(intent);
 
 
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.sign_in_button:
-                signIn();
-                break;
-            case R.id.logout_button:
-                signOut();
-                break;
-        }
-    }
-
-    private void signIn(){
-        Intent signIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signIntent,RC_SIGN_IN);
-
-    }
-    private void signOut(){
-        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
-            @Override
-            public void onResult(@NonNull Status status) {
-                statusTextView.setText("Bye");
-            }
-        });
-    }
-
-
-    private void handleSignResult(GoogleSignInResult result){
-        Log.d(TAG,"handleSignResult " + result.isSuccess());
-        if(result.isSuccess()){
-            GoogleSignInAccount acc = result.getSignInAccount();
-            statusTextView.setText("Zdarowa "+ acc.getDisplayName());
-        }
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Log.d(TAG,"onConnectionFailed " + connectionResult);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == RC_SIGN_IN){
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleSignResult(result);
-        }
-    }
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        mGoogleApiClient.connect();
-        AppIndex.AppIndexApi.start(mGoogleApiClient, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(mGoogleApiClient, getIndexApiAction());
-        mGoogleApiClient.disconnect();
     }
 }
--- app/src/main/java/com/examplenasa/roma/mytestapp/application/MyTestApplication.java
+++ app/src/main/java/com/examplenasa/roma/mytestapp/application/MyTestApplication.java
@@ -3,15 +3,35 @@ package com.examplenasa.roma.mytestapp.application;
 import android.app.Application;
 
 import com.facebook.FacebookSdk;
+import com.google.firebase.auth.TwitterAuthCredential;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
 
-/**
- * Created by valery on 05.11.16.
- */
+import io.fabric.sdk.android.Fabric;
+
 
 public class MyTestApplication extends Application {
+
+
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "Z4uq5htTrrQjVeac3Y3IzmN55";
+    private static final String TWITTER_SECRET = "v5tnRVggYbZFpX7Ofug2ZFCeDmFcnfLXkmD4QgTDnY90DILQFc";
+
 
     @Override
     public void onCreate() {
         FacebookSdk.sdkInitialize(this);
+
+
+     //   PsiMethod:onCreateTwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+   //  PsiMethod:onCreateFabric.with(this, new Twitter(authConfig));ig));
+
+
+
+
     }
 }
